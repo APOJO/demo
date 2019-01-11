aaf22c46c43dcfb0fba2de34820ed926e2af2be3
lmf <18911736507@163.com>
2019-01-11 18:57:55 +0800

    修改变量注入

--- src/main/java/com/iotplatform/utils/AuthUtil.java
+++ src/main/java/com/iotplatform/utils/AuthUtil.java
@@ -8,50 +8,52 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AuthUtil {
-	
-	private static NorthApiClient northApiClient = null;
 
-	public static String  authPath;
-	@Value("${authPath}")
-	public  void setAuthPath(String authPath) {
-		authPath = authPath;
-	}
+    private static NorthApiClient northApiClient = null;
 
-	public static NorthApiClient initApiClient() {
-		if (northApiClient != null) {
-			return northApiClient;
-		}
-		northApiClient = new NorthApiClient();
+    private static String authPath;
+
+
+    @Value("${authPath}")
+    public void setAuthPath(String authPath) {
+        AuthUtil.authPath = authPath;
+    }
+
+    public static NorthApiClient initApiClient() {
+        if (northApiClient != null) {
+            return northApiClient;
+        }
+        northApiClient = new NorthApiClient();
 
         PropertyUtil.init(authPath);
-		
-		ClientInfo clientInfo = new ClientInfo();
+
+        ClientInfo clientInfo = new ClientInfo();
         clientInfo.setPlatformIp(PropertyUtil.getProperty("platformIp"));
         clientInfo.setPlatformPort(PropertyUtil.getProperty("platformPort"));
         clientInfo.setAppId(PropertyUtil.getProperty("appId"));
 //        clientInfo.setSecret(PropertyUtil.getProperty("secret"));
         clientInfo.setSecret(getAesPropertyValue("secret"));
-        
+
         try {
-			northApiClient.setClientInfo(clientInfo);
-			northApiClient.initSSLConfig();
-		} catch (NorthApiException e) {
-			System.out.println(e.toString());
-		}        
-        
+            northApiClient.setClientInfo(clientInfo);
+            northApiClient.initSSLConfig();
+        } catch (NorthApiException e) {
+            System.out.println(e.toString());
+        }
+
         return northApiClient;
     }
-	
-	public static String getAesPropertyValue(String propertyName) {
-		String aesPwd = "123987"; //this is a test AES password
-        
+
+    public static String getAesPropertyValue(String propertyName) {
+        String aesPwd = "123987"; //this is a test AES password
+
 //      String originalProperty = "gPnTWO52yrobtjyobykkf12P8f4a";
 //      byte[] temp = AesUtil.encrypt(originalProperty, aesPwd);
 //      String hexStrResult = HexParser.parseByte2HexStr(temp);
 //      System.out.println("encrypted secret hex sting is ："  + hexStrResult);
-      
-		PropertyUtil.init(authPath);
-		byte[] secret = HexParser.parseHexStr2Byte(PropertyUtil.getProperty(propertyName));
-		return new String(AesUtil.decrypt(secret, aesPwd));
-	}
+
+        PropertyUtil.init(authPath);
+        byte[] secret = HexParser.parseHexStr2Byte(PropertyUtil.getProperty(propertyName));
+        return new String(AesUtil.decrypt(secret, aesPwd));
+    }
 }
